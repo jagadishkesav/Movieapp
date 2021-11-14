e1a6f80eb77ccbf4c784e128e6ca4ca00cf916c6
jagadishkesav <46987467+jagadishkesav@users.noreply.github.com>
2021-11-14 18:06:07 +0530

    insert login data

--- backend/controllers/login_controller.js
+++ backend/controllers/login_controller.js
@@ -1,6 +1,7 @@
 const { login } = require('../models');
 
 exports.login_post = async (req, res) => {
+    const { username, password, is_logged_in } = req.body
     await login.findOne({
         where: {
             username: req.body.username
@@ -9,18 +10,18 @@ exports.login_post = async (req, res) => {
         .then((result) => {
             if (result === null) return res.status(400).send('Invalid username and password');
             return login.create({
-                username: req.body.email,
+                username: username,
                 password: password,
-                is_logged_in: req.body.is_logged_in
+                is_logged_in: is_logged_in
             })
                 .then(() => {
                     res.status(200).send({
                         message: 'login successfully'
                     })
                 })
-                .catch((error) => res.status(400).send({ message: 'Unable to register' }));
+                .catch(() => res.status(400).send({ message: 'Unable to login' }));
         })
-        .catch(err => console.error(err))
+        .catch(err => console.error("something went wrong"))
 }
 
 
