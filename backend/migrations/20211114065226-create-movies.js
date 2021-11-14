5157e7bbbc1529c053b369afffcdb489b0cd1138
sneha d <70475588+snehadsea@users.noreply.github.com>
2021-11-14 12:29:20 +0530

    db creation

---
+++ backend/migrations/20211114065226-create-movies.js
@@ -0,0 +1,51 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('movies', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      ratings: {
+        type: Sequelize.NUMERIC
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      video: {
+        type: Sequelize.BOOLEAN
+      },
+      language_id: {
+        type: Sequelize.NUMBER
+      },
+      director_id: {
+        type: Sequelize.NUMBER
+      },
+      actor_id: {
+        type: Sequelize.NUMBER
+      },
+      release_date: {
+        type: Sequelize.DATE
+      },
+      overview: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('movies');
+  }
+};
